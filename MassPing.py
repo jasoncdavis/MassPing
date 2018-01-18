d0b1ea86deeb2775997df0e7fc26f084ab002f86
Jason C. Davis <jason@davisnet.org>
2018-01-18 15:24:13 -0500

    Update MassPing.py

--- MassPing.py
+++ MassPing.py
@@ -7,6 +7,8 @@ script loading a file with a list of devices to ping, then pings the devices, th
 the results into a webpage
 
 v1	2017-1217	jadavis Initial version
+v2  2018-0118	jadavis	Updated to tag influxdb data with hostname as supplied in devicelist;
+						also removed HTML web page creation in lieu of using Grafana
 """
 
 import shlex
@@ -21,65 +23,10 @@ import datetime
 
 ## User defined vars
 devicelist = "devicelist.txt"
-htmlcolumncount = 20 #Number of columns in a table
-warningratio = 0.5   #number of millisecond where a HIGHER number (ms) reports yellow/warning
 influxserver = "localhost" #hostname or IP of InfluxDB server
-databasename = "RTPNML"     #name of existing InfluxDB database
-dashboardfilename = "/var/www/html/availability.html"
+databasename = "CHANGEME"     #name of existing InfluxDB database
 ##
 
-## Other script variables
-starthtml = '''<html>
-<head>
-	<META HTTP-EQUIV="refresh" CONTENT="5" />
-	<title>CiscoLive NOC Device Availability Dashboard</title>
-</head>
-<style>
-
-body { font-size: 62.5%; }
-
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-
-td, th {
-    border: 1px solid #dddddd;
-    text-align: center;
-    padding: 8px;
-    font-size: 33.3%;
-}
-
-tr:nth-child(even) {
-    background-color: #dddddd;
-}
-
-.upgreen {
-	background-color: lime;
-	color: black;
-}
-
-.warnyellow {
-	background-color: yellow;
-	color: black;
-}
-
-.downred {
-	background-color: red;
-	color: white;
-}
-</style>
-</head>
-<body>
-
-<table>
-<tr>
-'''
-
-endtable = '''</tr>
-</table>
-'''
 
 
 def get_fping_output(cmd):
@@ -101,16 +48,16 @@ def load_devicefile():
 
 def getpingresults():
 	iplist = dict(load_devicefile())
-	cmd = "/usr/bin/fping -C 1 -A -d -q {}".format(" ".join(map(str, iplist.keys())))
+	cmd = "/usr/bin/fping -C 1 -A -q {}".format(" ".join(map(str, iplist.keys())))
 	exitcode, out, results = get_fping_output(cmd)
 	
 	pingresults = []
 	for aline in results.split("\n"):
+		#print('Working on line: {0}'.format(aline))
 		if aline:
-			m = re.match(r"(\S+) \((\S+)\)\s+\:\s(\S+)", aline)
-			hostname = m.group(1).replace('.rtpnml.cisco.com','')
-			ipaddress = m.group(2)
-			rtt = m.group(3)
+			m = re.match(r"(\S+)\s+:\s(\S+)", aline)
+			ipaddress = m.group(1)
+			rtt = m.group(2)
 			if rtt == '-':
 				iplist[ipaddress] += (float(9999),)
 			elif float(rtt) > warningratio:
@@ -118,43 +65,24 @@ def getpingresults():
 			else:
 				iplist[ipaddress] += (float(rtt),)
 	
-	sorted_iplist = sorted(iplist.items(), key=lambda k: k[1][3], reverse=True)
-	return sorted_iplist
+	#print(iplist)
+	return iplist
 
 def createtabledata():
-	ts = time.time()
-	timenow = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-	sorted_iplist = getpingresults()
-	html = starthtml
+	iplist = getpingresults()
 	influxdata = []
-	columncount = 0
-	influxlaunch = 'http://10.122.6.10:3000/dashboard/db/ping-results?panelId=1&fullscreen&orgId=1&from=now-1h&to=now&var-host='
-	for key in sorted_iplist:
+	for key, values in iplist.items():
+		#print(key, values)
+		#print(values[0], values[1], values[2], values[3])
 		# InfluxDB line protocol template...
-		# ping,host=<host>,location=<location> rtt=<value>
-		influxentry = "ping,host=" + key[0] + ",location=" + key[1][1] + ",function=" + key[1][2] + " rtt=" + str(key[1][3])
-		if key[1][3] == 9999:
-			#tabledata = '<td class="downred">{hostname}<br>{ipaddress}<br>DOWN!</td>'.format(hostname=key[1][0], ipaddress=key[0])
-			tabledata = '<td class="downred"><a href="{influxlaunch}{ipaddress}" target="_blank">{hostname}</a><br>{ipaddress}<br>DOWN!</td>'.format(influxlaunch=influxlaunch, hostname=key[1][0], ipaddress=key[0])
-		elif float(key[1][3]) > warningratio:
-			tabledata = '<td class="warnyellow"><a href="{influxlaunch}{ipaddress}" target="_blank">{hostname}</a><br>{ipaddress}<br>{rtt} ms</td>'.format(influxlaunch=influxlaunch, hostname=key[1][0], ipaddress=key[0], rtt=key[1][3])
-		else:
-			tabledata = '<td class="upgreen"><a href="{influxlaunch}{ipaddress}" target="_blank">{hostname}</a><br>{ipaddress}<br>{rtt} ms</td>'.format(influxlaunch=influxlaunch, hostname=key[1][0], ipaddress=key[0], rtt=key[1][3])
+		# ping,host=<host>,hostname=<hostname>,location=<location> rtt=<value>
+		influxentry = "ping,host=" + key + ",hostname=" + values[0] + ",location=" + values[1] + ",function=" + values[2] + " rtt=" + str(values[3])
 		influxdata.append(influxentry)
 		
-		if columncount == htmlcolumncount:
-			html = '\n'.join([html, '</tr>\n<tr>'])
-			columncount = 0
-		else:
-			html = '\n'.join([html, tabledata])
-		columncount += 1
-	
 	influxdata = '\n'.join(influxdata)
-	
-	fh = open(dashboardfilename, "w")
-	fh.write('\n'.join([html, endtable, "<p>Last Run:", timenow, "</p>", "</body>"]))
-	fh.close()
+	#print(influxdata)
 	return influxdata
+	
 
 def write2influx():
 	influxdata = createtabledata()
@@ -169,12 +97,18 @@ def write2influx():
 	
 def dowork():
 	timenow = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-	print("I'm working...", timenow)
+	start = datetime.datetime.now()
+	print("Started...", timenow)
 	write2influx()
-	timenow = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-	print("I'm done...", timenow)
+	end = datetime.datetime.now()
+	elapsed = end - start
+	#print(elapsed.seconds,":",elapsed.microseconds) 
+
+	#timenow = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+	print("   Done... {0}.{1}sec".format(elapsed.seconds, elapsed.microseconds))
 	
-schedule.every(15).seconds.do(dowork)
+	
+schedule.every(10).seconds.do(dowork)
 
 while 1:
 	schedule.run_pending()
